bb9b3f2b8760044aa47c341612c09416491f1174
Yuushi <68327352+realYuushi@users.noreply.github.com>
2020-08-01 23:23:44 +0200

    Updated to v0.1.1

--- src/main/java/com/yuushi/uuidmod/UUIDMod.java
+++ src/main/java/com/yuushi/uuidmod/UUIDMod.java
@@ -11,7 +11,7 @@ public class UUIDMod
 {
     public static final String MODID = "uuidmod";
     public static final String MOD_NAME = "UUIDMod";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.1.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
