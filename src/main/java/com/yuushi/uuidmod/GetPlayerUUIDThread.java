0bf592a4b1ef350b3a131295ed0bcc5309e85e12
Tymanyay <32660892+Tymanyay@users.noreply.github.com>
2020-08-02 09:10:55 -0600

    Fixed references and formatted better

--- src/main/java/com/yuushi/uuidmod/GetPlayerUUIDThread.java
+++ src/main/java/com/yuushi/uuidmod/GetPlayerUUIDThread.java
@@ -1,7 +1,7 @@
 package com.yuushi.uuidmod;
 
 import com.google.gson.Gson;
-
+import com.yuushi.uuidmod.utils.HttpRequest;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.event.ClickEvent;
 import net.minecraft.event.HoverEvent;
@@ -13,11 +13,9 @@ public class GetPlayerUUIDThread implements Runnable {
 
 	static ICommandSender sender = null;
 	static String[] args;
-	
+
 	@Override
 	public void run() {
-		
-		// TODO Auto-generated method stub
 		String name = args[0];
 		String url = "https://api.ashcon.app/mojang/v2/user/" + name;
 		String content = HttpRequest.get(url).body();
@@ -37,23 +35,22 @@ public class GetPlayerUUIDThread implements Runnable {
 			ChatStyle uuid_r_style = new ChatStyle();
 			uuid_r_style.setChatClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/copy " + uuid_r));
 			uuid_r_style.setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(EnumChatFormatting.GRAY + "Click to copy raw UUID.")));
-			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_PURPLE + "" + EnumChatFormatting.BOLD + "" + EnumChatFormatting.STRIKETHROUGH + "----------------------------------" ));
-			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_PURPLE + "" + EnumChatFormatting.BOLD + username + EnumChatFormatting.LIGHT_PURPLE + "'s UUID" ));
+			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_PURPLE + "" + EnumChatFormatting.BOLD + "" + EnumChatFormatting.STRIKETHROUGH + "----------------------------------"));
+			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_PURPLE + "" + EnumChatFormatting.BOLD + username + EnumChatFormatting.LIGHT_PURPLE + "'s UUID"));
 			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.LIGHT_PURPLE + "> UUID:"));
 			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "  " + uuid).setChatStyle(uuid_style));
 			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.LIGHT_PURPLE + "> Raw UUID:"));
 			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "  " + uuid_r).setChatStyle(uuid_r_style));
-			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_PURPLE + "" + EnumChatFormatting.BOLD + "" + EnumChatFormatting.STRIKETHROUGH + "----------------------------------" ));
+			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_PURPLE + "" + EnumChatFormatting.BOLD + "" + EnumChatFormatting.STRIKETHROUGH + "----------------------------------"));
 		}
 
-		
 	}
-	
+
 	public static void main(ICommandSender isender, String[] strings) {
 		sender = isender;
 		args = strings;
-		GetPlayerUUIDThread obj = new GetPlayerUUIDThread();  
-	    Thread tobj =new Thread(obj);  
-	    tobj.start();
+		GetPlayerUUIDThread obj = new GetPlayerUUIDThread();
+		Thread tobj = new Thread(obj);
+		tobj.start();
 	}
 }
