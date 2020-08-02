85e2fe2f1ebb98b7a5ad082121ffb26d968b3ff2
Tymanyay <32660892+Tymanyay@users.noreply.github.com>
2020-08-02 09:11:32 -0600

    Formatting

--- src/main/java/com/yuushi/uuidmod/UUIDMod.java
+++ src/main/java/com/yuushi/uuidmod/UUIDMod.java
@@ -6,17 +6,15 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = UUIDMod.MODID, name= UUIDMod.MOD_NAME, version = UUIDMod.VERSION)
-public class UUIDMod
-{
-    public static final String MODID = "uuidmod";
-    public static final String MOD_NAME = "UUIDMod";
-    public static final String VERSION = "0.1.1";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-    	ClientCommandHandler.instance.registerCommand(new UUIDCommand(this));
-    	ClientCommandHandler.instance.registerCommand(new CopyCommand(this));
-    }
+@Mod(modid = UUIDMod.MODID, name = UUIDMod.MOD_NAME, version = UUIDMod.VERSION)
+public class UUIDMod {
+	public static final String MODID = "uuidmod";
+	public static final String MOD_NAME = "UUIDMod";
+	public static final String VERSION = "0.1.1";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		ClientCommandHandler.instance.registerCommand(new UUIDCommand(this));
+		ClientCommandHandler.instance.registerCommand(new CopyCommand(this));
+	}
 }
