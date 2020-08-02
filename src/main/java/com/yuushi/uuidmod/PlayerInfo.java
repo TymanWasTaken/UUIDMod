70df5eb60a856e25a991ef6f7b06ed5c2d29a97f
Tymanyay <32660892+Tymanyay@users.noreply.github.com>
2020-08-02 09:12:30 -0600

    Small formatting

--- src/main/java/com/yuushi/uuidmod/PlayerInfo.java
+++ src/main/java/com/yuushi/uuidmod/PlayerInfo.java
@@ -2,17 +2,16 @@ package com.yuushi.uuidmod;
 
 import com.yuushi.uuidmod.UUIDCommand;
 
-
 public class PlayerInfo {
 	private String uuid;
 	private String username;
-	
+
 	public String getUUID() {
 		return uuid;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 }
