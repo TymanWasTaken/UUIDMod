991862caeb485b6ad15fd2ead6a511f372c9d1bb
Tymanyay <32660892+Tymanyay@users.noreply.github.com>
2020-08-02 09:18:32 -0600

    Formatting

--- src/main/java/com/yuushi/uuidmod/commands/UUIDCommand.java
+++ src/main/java/com/yuushi/uuidmod/commands/UUIDCommand.java
@@ -14,39 +14,34 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 
-
 public class UUIDCommand extends CommandBase {
-	
+
 	UUIDMod uuidmod;
 
-    public UUIDCommand(UUIDMod uuidmod) {
-        this.uuidmod = uuidmod;
-    }
-    
+	public UUIDCommand(UUIDMod uuidmod) {
+		this.uuidmod = uuidmod;
+	}
+
 	@Override
 	public String getCommandName() {
-		// TODO Auto-generated method stub
 		return "uuid";
 	}
 
 	@Override
 	public String getCommandUsage(ICommandSender sender) {
-		// TODO Auto-generated method stub
 		return "uuid <username>";
 	}
-	
+
 	@Override
-    public boolean canCommandSenderUseCommand(ICommandSender sender)
-    {
-        return true;
-    }
-	
+	public boolean canCommandSenderUseCommand(ICommandSender sender) {
+		return true;
+	}
+
 	@Override
 	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
 		if (args.length < 1 || args.length > 1) {
 			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "[UUIDMod] Usage: /uuid <username>"));
-            return;
+			return;
 		}
 		else {
 			GetPlayerUUIDThread.main(sender, args);
