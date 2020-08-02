7ccc7faafde77c834a4522bdf6a278afd3df86d3
Tymanyay <32660892+Tymanyay@users.noreply.github.com>
2020-08-02 09:13:57 -0600

    Update formatting

--- src/main/java/com/yuushi/uuidmod/commands/CopyCommand.java
+++ src/main/java/com/yuushi/uuidmod/commands/CopyCommand.java
@@ -11,45 +11,40 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 
 public class CopyCommand extends CommandBase {
-	
+
 	UUIDMod uuidmod;
 
-    public CopyCommand(UUIDMod uuidmod) {
-        this.uuidmod = uuidmod;
-    }
-    
-	@Override
-	public String getCommandName() {
-		// TODO Auto-generated method stub
-		return "umcopy";
-	}
-	
-	@Override
-	public String getCommandUsage(ICommandSender sender) {
-		// TODO Auto-generated method stub
-		return "umcopy <text>";
-	}
-	
-	@Override
-    public boolean canCommandSenderUseCommand(ICommandSender sender)
-    {
-        return true;
-    }
-	
-	@Override
-	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
-		if (args.length < 1 || args.length > 1) {
-            return;
-		}
-		else {
-		String text = args[0];
-		StringSelection stringSelection = new StringSelection(text);
-		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-		clipboard.setContents(stringSelection, null);
-		sender.addChatMessage(new ChatComponentText(EnumChatFormatting.WHITE + "Copied to clipboard"));
-		}
+	public CopyCommand(UUIDMod uuidmod) {
+		this.uuidmod = uuidmod;
 	}
 
+	@Override
+	public String getCommandName() {
+		return "umcopy";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		return "umcopy <text>";
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender sender) {
+		return true;
+	}
+
+	@Override
+	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+		if (args.length < 1 || args.length > 1) {
+			return;
+		}
+		else {
+			String text = args[0];
+			StringSelection stringSelection = new StringSelection(text);
+			Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			clipboard.setContents(stringSelection, null);
+			sender.addChatMessage(new ChatComponentText(EnumChatFormatting.WHITE + "Copied to clipboard"));
+		}
+	}
 
 }
